11c323d4d6b4fa59e062743c7f0e66992a2da3ac
Björn Linse <bjorn.linse@gmail.com>
2018-03-02 21:28:45 +0100

    screen/ui: use line-based rather than char-based updates
    Add ext_multigrid and ext_hlstate (the later mostly nonfunctional)
    and reimplement old put, update_fg, eol_clear etc events in remote layer

--- test/functional/api/vim_spec.lua
+++ test/functional/api/vim_spec.lua
@@ -1121,6 +1121,8 @@ describe('api', function()
           ext_popupmenu = false,
           ext_tabline = false,
           ext_wildmenu = false,
+          ext_multigrid=false,
+          ext_hlstate=false,
           height = 4,
           rgb = true,
           width = 20,
@@ -1131,17 +1133,9 @@ describe('api', function()
       screen:detach()
       screen = Screen.new(44, 99)
       screen:attach({ rgb = false })
-      expected = {
-        {
-          ext_cmdline = false,
-          ext_popupmenu = false,
-          ext_tabline = false,
-          ext_wildmenu = false,
-          height = 99,
-          rgb = false,
-          width = 44,
-        }
-      }
+      expected[1].rgb = false
+      expected[1].width = 44
+      expected[1].height = 99
       eq(expected, nvim("list_uis"))
     end)
   end)
