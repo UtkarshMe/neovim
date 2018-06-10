3609993efa7347ea75f2d6925286300b4c73b4a1
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-06-11 01:26:53 +0530

    Allow UIs to set grid size different from window size

--- src/nvim/types.h
+++ src/nvim/types.h
@@ -40,8 +40,9 @@ typedef struct {
   int OffsetRow;
   int OffsetColumn;
 
-  int internal_Rows;
-  int internal_Columns;
+  // the size expected to be allocated to the internal grid
+  int internal_rows;
+  int internal_columns;
 
   int was_resized;
 } ScreenGrid;
