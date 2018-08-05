caade9e03c8b86a6854045bc6b2704c97005f02a
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-08-05 13:11:19 +0530

    tests: Change API version for ext-window events

--- src/nvim/api/ui_events.in.h
+++ src/nvim/api/ui_events.in.h
@@ -85,26 +85,26 @@ void grid_destroy(Integer grid)
   FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_position(Integer win, Integer grid, Integer startrow,
                   Integer startcol, Integer width, Integer height)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 
 void win_split(Integer win1, Integer grid1, Integer win2, Integer grid2, Integer flags)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_move_cursor(Integer direction, Integer count)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_exchange(Integer win, Integer grid, Integer count)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_resize_equal (void)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_close(Integer win, Integer grid)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_rotate(Integer win, Integer grid, Integer direction, Integer count)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_move(Integer win, Integer grid, Integer flags)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_height_set(Integer win, Integer grid, Integer height)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 void win_width_set(Integer win, Integer grid, Integer width)
-  FUNC_API_SINCE(4) FUNC_API_REMOTE_ONLY;
+  FUNC_API_SINCE(5) FUNC_API_REMOTE_ONLY;
 
 void popupmenu_show(Array items, Integer selected,
                     Integer row, Integer col, Integer grid)
